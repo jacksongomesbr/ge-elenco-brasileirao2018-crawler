e66d738a8a4d6419d5b4cdc6482bbd4cd8dd2c8a
Jackson Gomes <jgomes@ceulp.edu.br>
2018-04-13 01:39:42 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ge-elenco-brasileirao2018-crawler
 Crawler ["simples"] para obtenção de dados da "Avaliação dos elencos do Brasileirão 2018" feita pelo GloboEsporte.com
+
+Por volta do início de abril de 2018 o GloboEsporte.com publicou um conteúdo especial contendo uma avaliação dos principais jogadores dos 20 times da elite do futebol brasileiro no endereço http://app.globoesporte.globo.com/futebol/brasileirao-serie-a/guia/avaliacao-dos-elencos-brasileirao-2018.
+
+Segundo descrição da própria publicação:
+
+> O Brasileirão está começando, e o GloboEsporte.com volta a avaliar os principais jogadores dos 20 clubes da elite. Vale lembrar que o momento vivido pelo atleta conta na hora de escolher seu status.
+
+Este trabalho resultou em uma avaliação (pontuação) de 23 jogadores de cada um dos times conforme as categorias:
+
+* É craque (6 pontos)
+* Joga muito (4 pontos)
+* Agrega valor (3 pontos)
+* Compõem elenco (2 pontos)
+* Sujeito a vaias (1 ponto)
+* Irrita a torcida (não pontua)
+
+A partir disso foi criado um ranking dos times. Neste momento (13/04/2018) o Palmeiras é o time com maior pontuação (79) e o Paraná, com a menor (40).
+
+A publicação não informa mais detalhes do processo de avaliação (quem foram os avaliadores, qual o período de avaliação etc.), mas informa que foi considerada a situação atual de cada jogador.
+
+Este repositório é um crawler para os dados desta publicação e foi criado, inicialmente, como um exercício de desenvolvimento de crawler em Python. Para isso utiliza [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/) e [Requests](http://docs.python-requests.org/en/master/).
+
+Ao ser executado o programa `main.py` gera quatro arquivos na pasta local:
+
+* `evaluation_criterion.csv`: armazena os critérios de avaliação; colunas: `id`, `label`
+* `players_positions.csv`: armazena as posições dos jogadores; colunas: `id`, `label`
+* `teams.csv`: armazena dados dos times; colunas: `id`, `name`, `description`, `stats_1`, `stats_2`, `stats_3`, `stats_4`, `stats_5`, `stats_6` (cada coluna `stats_n` representa a quantidade de jogadores do critério `n`)
+* `players_evaluations.csv`: armazena dados de avaliações dos jogadores; colunas: `team`, `id`, `name`, `photo_url`, `position`, `evaluation`
+
+Este repositório já inclui estes arquivos (gerados em 13/04/2018 por volta de 01:00:00, horário de Brasília).
+
+Quem sabe o pessoal do GloboEsporte.com continua avaliando esses jogadores a cada rodada do Brasileirão 2018 e poderemos ver esses dados gerarem um *lindo* conjunto de dados para análises mais elaboradas =)
+
+> **Aviso**: o código não está comentado, nem modularizado e nem faz tratamentos, por isso use por sua conta e risco :p
+
---
+++ main.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+
+player_positions = {}
+evaluation_criterion = {}
+teams = {}
+
+url = 'http://app.globoesporte.globo.com/futebol/brasileirao-serie-a/guia/avaliacao-dos-elencos-brasileirao-2018'
+r = requests.get(url)
+enc = r.encoding
+html = str(r.text.encode(enc), 'utf-8')
+
+bs = BeautifulSoup(html, "lxml")
+team_details = bs.select('.team-details-container')
+for team_detail in team_details:
+    team_id = team_detail.attrs['data-team']
+    title = team_detail.select_one('.team-about-title').text
+    description = team_detail.select_one('.team-about-description').text
+    big_value = team_detail.select_one('.big-value').text
+    if team_id not in teams:
+        teams[team_id] = {
+            'name': title,
+            'description': description,
+            'value': big_value,
+            'players': {},
+            'stats': {}
+        }
+    for player in team_detail.select('.team-players-list-item'):
+        id = player.attrs['data-jogador']
+        position_code = player.attrs['data-position']
+        criteria_id = player.attrs['data-criteria-id']
+        photo = player.select_one('.team-players-list-item-image').attrs['src']
+        position_name = player.select_one('.player-position').text
+        name = player.select_one('.player-name').text
+        criteria_label = player.select_one('.player-evaluation-label').text
+        if position_code not in player_positions:
+            player_positions[position_code] = position_name
+        if criteria_id not in evaluation_criterion:
+            evaluation_criterion[criteria_id] = criteria_label
+        if criteria_id not in teams[team_id]['stats']:
+            teams[team_id]['stats'][criteria_id] = 0
+        teams[team_id]['players'][id] = {
+            'name': name,
+            'photo': photo,
+            'position_code': position_code,
+            'criteria_id': criteria_id,
+        }
+        for cid in evaluation_criterion:
+            if criteria_id == cid:
+                teams[team_id]['stats'][cid] += 1
+
+for id in evaluation_criterion:
+    for team in teams:
+        if id not in teams[team]['stats']:
+            teams[team]['stats'][id] = 0
+
+with open('evaluation_criterion.csv', 'w', encoding='utf-8') as f:
+    f.write('"id","label"\n')
+    for id, label in evaluation_criterion.items():
+        f.write('"{}","{}"\n'.format(id, label))
+
+with open('players_positions.csv', 'w', encoding='utf-8') as f:
+    f.write('"id","label"\n')
+    for id, label in player_positions.items():
+        f.write('"{}","{}"\n'.format(id, label))
+
+with open('teams.csv', 'w', encoding='utf-8') as f:
+    headers = ['id','name','description']
+    for id in evaluation_criterion:
+        headers.append('stats_{}'.format(id))
+    f.write(','.join(['"{}"'.format(h) for h in headers]) + '\n')
+    for team in teams:
+        line = [team, teams[team]['name'], teams[team]['description']]
+        for id in evaluation_criterion:
+            line.append(teams[team]['stats'][id])
+        f.write(','.join(['"{}"'.format(item) for item in line]) + '\n')
+
+with open('players_evaluations.csv', 'w', encoding='utf-8') as f:
+    f.write('"team","id","name","photo_url","position","evaluation"\n')
+    for team in teams:
+        for id, player in teams[team]['players'].items():
+            f.write('"{}","{}","{}","{}","{}","{}"\n'.format(team, id, player['name'], player['photo'], player['position_code'], player['criteria_id']))
